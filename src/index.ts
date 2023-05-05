3054054e272c240ec4956ea5d6bff17d706df8bd
Tristan F <LeoDog896@hotmail.com>
2023-05-05 20:28:11 +0000

    feat: setup tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import { Project } from "ts-morph";
 
 const project = new Project();
+
+project.createSourceFile("index.ts", "type X<N extends number> = [  N,    N]")
+
+// we want to format the file, then get the new output
+project.getSourceFileOrThrow("index.ts").formatText()
+
+const output = project.getSourceFileOrThrow("index.ts").getText()
+console.log(output)
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+import "../src/index.js"
+
+test('does stuff', () => {
+    console.log(1)
+})
